2211fb0513540a89471f30507ed5191df5818b7c
Jonas <web@emd.art.br>
2021-07-25 20:43:30 -0300

    corregindo styled component

--- src/view/Home/style.ts
+++ src/view/Home/style.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+
+
+
+
 export const Container = styled.div`
   width: 80%;
   margin: auto;
@@ -16,21 +20,44 @@ export const Container = styled.div`
     }
   }
 
-  section {
+   section {     
     height: 100vh;
     width: 100%;
-    background: #e2e2;
+    margin-top: 10%;
+    background-color: #eaeaea;
     display: flex;
     justify-content: space-around;
 
     .product-content{
-      display: grid;
+      display: blcok;
       text-align: center;
       height: 300px;
+      padding-top: 50%
+      margint-bottom: -80%
       background: #fff;
       border-radius: 12px;
       padding: 12px;
 
     }
+    .logo{
+            width: 20px;
+            margin: 20px;
+        }    
+
+    descricao {
+      width: 100%
+      heigth: 50%
+    }
+
   }
+`
+
+export const Form = styled.div`
+    form {
+      margin-left: 50%
+      width: 100% !import;
+      
+      
+    }
+
 `
